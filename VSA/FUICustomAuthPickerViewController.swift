b1411f35c4de372ded947b8066f141b101eecbdc
Amerterasu <le.p.khoa@gmail.com>
2018-08-17 12:39:30 -0600

    refactor the code related to creating the background

--- VSA/FUICustomAuthPickerViewController.swift
+++ VSA/FUICustomAuthPickerViewController.swift
@@ -10,33 +10,36 @@ import Foundation
 import FirebaseUI
 
 class FUICustomAuthPickerViewController: FUIAuthPickerViewController{
+    let screenWidth = UIScreen.main.bounds.size.width
+    let screenHeight = UIScreen.main.bounds.size.height
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //customize the background
+        setBackgroundImage(backgroundImageName: "mountain")
+        addBlurEffect()
         
-//        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-//        self.navigationController?.navigationBar.shadowImage = UIImage()
-//        self.navigationController?.navigationBar.isTranslucent = true
-//        self.navigationController?.view.backgroundColor = UIColor.clear
-//        self.navigationController?.navigationBar.backgroundColor = UIColor.clear
-        
-        let width = UIScreen.main.bounds.size.width
-        let height = UIScreen.main.bounds.size.height
-        
-        //setting background image
-        let imageViewBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
-        imageViewBackground.image = UIImage(named: "mountain")
-        imageViewBackground.contentMode = UIViewContentMode.scaleAspectFill
-        view.insertSubview(imageViewBackground, at: 0)
-        
-        //setting the blur effect
-        let blurView = UIVisualEffectView()
-        blurView.frame = self.view.frame
-        blurView.frame = CGRect(x: 0, y: 0, width: width, height: height)
-        blurView.effect = UIBlurEffect(style: .regular)
-        view.insertSubview(blurView, at: 1)
         
         //TODO: add swuvsa logo
         //TODO: add constraints
         
+    }
+    
+    func setBackgroundImage(backgroundImageName: String){
+        //setting background image
+        let imageViewBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight))
+        imageViewBackground.image = UIImage(named: backgroundImageName)
+        imageViewBackground.contentMode = UIViewContentMode.scaleAspectFill
+        view.insertSubview(imageViewBackground, at: 0)
+    }
+    
+    func addBlurEffect(){
+        //setting the blur effect
+        let blurView = UIVisualEffectView()
+        blurView.frame = self.view.frame
+        blurView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        blurView.effect = UIBlurEffect(style: .regular)
+        view.insertSubview(blurView, at: 1)
     }
 }
